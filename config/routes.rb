7a690f2bf9094504304a5c12d6f833a14050fac9
Oscar Knoops <oscarknoops@yahoo.com>
2018-12-30 17:33:34 +0100

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :admins
   root to: 'pages#home'
   get 'aboutus', to: 'pages#aboutus'
+  get 'store', to: 'pages#store'
+  get 'community', to: 'pages#community'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
